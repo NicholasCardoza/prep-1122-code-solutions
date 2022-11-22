7fc23270ee0e73b689f700ad36e361d0f01a7bba
Nicholas Cardoza <Nicholascardoza84@yahoo.com>
2022-11-22 18:42:04 +0000

    completed prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,21 @@
+var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+
+console.log('languages array', languages);
+
+var firstElement = languages[0];
+
+console.log('The 1st element of the array is:', firstElement);
+
+var thirdElement = languages[2];
+
+console.log('The 3rd element of the array is:', thirdElement);
+
+var length = languages.length;
+
+console.log('The length of the array is:', length);
+
+var lastIndex = length - 1;
+
+var lastElement = languages[lastIndex];
+
+console.log('The last element of the array is:', lastElement);
