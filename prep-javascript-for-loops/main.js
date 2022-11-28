0635144a3b009739ed4fe7f4c98d82949ac9decb
Nicholas Cardoza <Nicholascardoza84@yahoo.com>
2022-11-28 17:52:46 +0000

    completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,11 @@
+for (var x = 0; x < 10; x++) {
+  console.log(x);
+}
+
+for (var n = 0; n < 20; n += 2) {
+  console.log(n);
+}
+
+for (var e = 100; e > 0; e--) {
+  console.log('Time Till EXPLOSION:' + ' ' + e + '!');
+}
